c539c5f8874af6edf545a453d3e60f812513e148
Laetitia Massa <laetitiamassa@gmail.com>
2016-07-17 01:51:27 +0200

    fixed syntax in devise parameters

--- config/initializers/devise_permitted_parameters.rb
+++ config/initializers/devise_permitted_parameters.rb
@@ -8,8 +8,8 @@ module DevisePermittedParameters
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up) << :name
-    devise_parameter_sanitizer.permit(:account_update) << :name
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :email, :name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :email, :name])
   end
 
 end
