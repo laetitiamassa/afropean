a0e6d69a9666a8de248ed13ac121fda095374371
Laetitia Massa <laetitiamassa@gmail.com>
2016-07-16 23:51:03 +0200

    fixing assets in prod

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Afropean
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    #for images in heroku
-    config.serve_static_assets = true
+    
   end
 end
