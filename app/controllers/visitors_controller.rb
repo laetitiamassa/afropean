f391eaac85310086379107cfba3b654cc3b3a59f
Laetitia Massa <laetitiamassa@gmail.com>
2016-07-17 09:42:28 +0200

    designed events

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,5 +1,6 @@
 class VisitorsController < ApplicationController
 	def home
+		@events = Event.active.first(3)
 	end
 
 	def about
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,8 @@
 class Event < ActiveRecord::Base
+
+	scope :active, lambda {where('start >= ?', Time.now)}
+
+	def past
+		start < Time.now
+	end
 end
